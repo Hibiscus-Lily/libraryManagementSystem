702730687026fa0cee9262251ade94cc7f21cc27
木槿 <2743334048@qq.com>
2022-11-02 16:52:17 +0800

    添加借阅查询

--- src/main/java/com/mujin/librarymanagementsystem/controller/Admin_BorrowController.java
+++ src/main/java/com/mujin/librarymanagementsystem/controller/Admin_BorrowController.java
@@ -1,9 +1,18 @@
 package com.mujin.librarymanagementsystem.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.mujin.librarymanagementsystem.common.constant.Code;
+import com.mujin.librarymanagementsystem.common.entity.Result;
+import com.mujin.librarymanagementsystem.pojo.BorrowInformationPojo;
 import com.mujin.librarymanagementsystem.service.BorrowInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
@@ -20,6 +29,39 @@ public class Admin_BorrowController {
     @Autowired
     public void setBorrowInformationService(BorrowInformationService borrowInformationService) {
         this.borrowInformationService = borrowInformationService;
+    }
+
+    /**
+     * 获取全部书籍的相关信息
+     */
+    @GetMapping("/getAllBorrowingRecords")
+    public Result getAllBookInformation(@RequestParam Integer page, @RequestParam Integer limit) {
+        PageHelper.startPage(page, limit);
+        List<BorrowInformationPojo> book = borrowInformationService.findAllBorrowingRecords();
+        PageInfo<BorrowInformationPojo> pageInfo = new PageInfo<>(book);
+        return new Result(Code.OK, pageInfo, "数据获取成功");
+    }
+
+    /**
+     * 获取全部书籍的相关信息
+     */
+    @GetMapping("/getAllBorrowingRecordsForBookTile")
+    public Result findAllBorrowingRecordsForBookTile(@RequestParam Integer page, @RequestParam Integer limit, @RequestParam String title) {
+        PageHelper.startPage(page, limit);
+        List<BorrowInformationPojo> book = borrowInformationService.findAllBorrowingRecordsForBookTile(title);
+        PageInfo<BorrowInformationPojo> pageInfo = new PageInfo<>(book);
+        return new Result(Code.OK, pageInfo, "数据获取成功");
+    }
+
+    /**
+     * 获取全部书籍的相关信息
+     */
+    @GetMapping("/getAllBorrowingRecordsForUserAccount")
+    public Result findAllBorrowingRecordsForUserAccount(@RequestParam Integer page, @RequestParam Integer limit, @RequestParam String account) {
+        PageHelper.startPage(page, limit);
+        List<BorrowInformationPojo> book = borrowInformationService.findAllBorrowingRecordsForUserAccount(account);
+        PageInfo<BorrowInformationPojo> pageInfo = new PageInfo<>(book);
+        return new Result(Code.OK, pageInfo, "数据获取成功");
     }
 
 
